30682cb3d1f04619cf7f6cbb9df52c0fd6986495
JayCompiler <51164500059@stu.ecnu.edu.cn>
2018-03-13 13:28:02 +0800

    third commit LeetCode

--- Leetcode/src/solution/hundred_twohundred/One1One.java
+++ Leetcode/src/solution/hundred_twohundred/One1One.java
@@ -11,8 +11,6 @@ public class One1One {
 
 	public static void main(String[] args) {
 		TreeNode treeNode = new TreeNode(-2);
-		//treeNode.left = new TreeNode(1);
-		//treeNode.right = new TreeNode(3);
 		int min=minDepth(treeNode);
 		System.out.println(min);
 	}
